3e1d171cae39cd590995264ab0d16bc06f7ac037
Monikakadalii <kadalimonika150@gmail.com>
2023-10-29 19:42:09 +0530

    prod

--- src/main/java/com/monika/recipe/repository/RecipeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.monika.recipe.repository;
-
-import com.monika.recipe.model.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {}
